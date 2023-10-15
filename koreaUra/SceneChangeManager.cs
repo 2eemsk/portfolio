621d0f0f391a2b5ff59fce2111a3f176f9e9f5dd
2eemsk <alstjr5892@gmail.com>
2023-10-16 02:49:44 +0900

    코레아우라! 프로젝트

---
+++ koreaUra/SceneChangeManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeManager : MonoBehaviour
+{
+     void Update()
+    {
+
+    }
+
+    public void ChangePlayScene()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void ChangeOpeningVideoScene()
+    {
+        SceneManager.LoadScene("OpeningVideoScene");
+    }
+
+    public void ChangeGameScene()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+
+    public void ChangeSettingScene()
+    {
+        if(Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("SettingScene");
+        }
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+        Debug.Log("Get Out This Game");
+    }
+}
